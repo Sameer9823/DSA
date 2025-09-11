5bd8e6ca461b58893fac1ea2f1f8108a6f43d713
Sameer Selokar <sameerselokar924@gmail.com>
2025-09-11 14:36:52 +0530

    Add Even class and LeetCode2 main class to count even digit numbers

---
+++ Array/LeetCode2.java
@@ -0,0 +1,22 @@
+class Even{
+    public int evenN(int[] nums){
+        int count = 0;
+
+        for(int num: nums){
+            int digit = String.valueOf(num).length();
+            if(digit % 2 ==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+public class LeetCode2 {
+    public static void main(String[] args) {
+        Even e = new Even();
+        int[] nums = {12,345,2,6,7896};
+        System.out.println(e.evenN(nums));
+    }
+    
+}
